ab8b7e19d4f9e43fa2fdea406927f2e8cf853c6d
CAILLOL Adrien <adrien.caillol@mailoo.org>
2018-04-30 09:29:48 +0200

    Réorganisation de la structure de la solution

---
+++ GameOfLife.Tests/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameOfLife.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMortUneCellule()
+        {
+            var tableau = new List<List<char>>
+            {
+                "........".ToCharArray().ToList(),
+                "........".ToCharArray().ToList(),
+                "..*.....".ToCharArray().ToList(),
+                "........".ToCharArray().ToList()
+            };
+
+            var jeuDeLaVie = new JeuDeLaVie(tableau);
+
+            List<List<char>> tableauResultat = jeuDeLaVie.JouerEtapeSuivante();
+
+            Assert.AreEqual('.', tableauResultat[2][2]);
+        }
+
+        [TestMethod]
+        public void TestMortUneCelluleAvecUneVoisine()
+        {
+            var tableau = new List<List<char>>
+            {
+                "........".ToCharArray().ToList(),
+                "........".ToCharArray().ToList(),
+                "..**....".ToCharArray().ToList(),
+                "........".ToCharArray().ToList()
+            };
+
+            var jeuDeLaVie = new JeuDeLaVie(tableau);
+
+            List<List<char>> tableauResultat = jeuDeLaVie.JouerEtapeSuivante();
+
+            Assert.AreEqual('.', tableauResultat[2][2]);
+        }
+
+        [TestMethod]
+        public void TestResteEnVieUneCelluleAvecDeuxVoisineHorizontales()
+        {
+            var tableau = new List<List<char>>
+            {
+                "........".ToCharArray().ToList(),
+                "........".ToCharArray().ToList(),
+                ".***....".ToCharArray().ToList(),
+                "........".ToCharArray().ToList()
+            };
+
+            var jeuDeLaVie = new JeuDeLaVie(tableau);
+
+            List<List<char>> tableauResultat = jeuDeLaVie.JouerEtapeSuivante();
+
+            Assert.AreEqual('*', tableauResultat[2][2]);
+        }
+    }
+
+}
